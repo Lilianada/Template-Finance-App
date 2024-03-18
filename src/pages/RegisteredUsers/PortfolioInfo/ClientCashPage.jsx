4768cb24561e9613544ce527c9a42f7619278b1e
Lilianada <lilibest30.lb@gmail.com>
2024-03-18 08:21:35 -0400

    cash transactions

--- src/pages/RegisteredUsers/PortfolioInfo/ClientCashPage.jsx
+++ src/pages/RegisteredUsers/PortfolioInfo/ClientCashPage.jsx
@@ -36,8 +36,8 @@ export default function ClientCashPage() {
   };
 
   const handleDelete = (id) => {
-    setSelectedId(id)
-    console.log(selectedId)
+    setSelectedId(id);
+    console.log(selectedId);
     customModal({
       showModal,
       title: "Are you sure?",
@@ -123,95 +123,100 @@ export default function ClientCashPage() {
         </div>
       </div>
       <div className="-mx-4 mt-8 sm:-mx-0">
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead>
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-              >
-                Type
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Amount
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-              >
-                Status
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Reference
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-              >
-                Date
-              </th>
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          {cashTransaction === 0 ? (<p>No Cash Transactions available.</p>
-            ) : (
-          <tbody className="divide-y divide-gray-200 bg-white text-left">
-            {cashTransaction.map((item, index) => (
-              <tr key={index}>
-                <td className="w-2/5 max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0 capitalize w-">
-                  {item.type}
-                  <dl className="font-normal lg:hidden">
-                    <dt className="sr-only sm:hidden">Status</dt>
-                    <dd className="mt-1 truncate text-gray-700 sm:hidden">
-                      {item.status}
-                    </dd>
-                    <dt className="sr-only lg:hidden">Reference</dt>
-                    <dd className="mt-1 truncate text-gray-500 lg:hidden">
-                      {item.reference}
-                    </dd>
-                  </dl>
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500 ">
-                  {formatNumber(item.amount)}
-                </td>
-                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell capitalize">
-                  {item.status}
-                </td>
-                <td className="hidden px-3 py-4 text-sm text-gray-500  capitalize">
-                  {item.reference}
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500 capitalize">
-                  {item.date}
-                </td>
-                <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                  <button
-                    onClick={() => handleEdit(item.id)}
-                    className="text-indigo-600 hover:text-indigo-900"
-                  >
-                    Edit<span className="sr-only">, {item.amount}</span>
-                  </button>
-                </td>
-                <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                  <button
-                    onClick={() => handleDelete(item.id)}
-                    className="text-red-600 hover:text-red-900"
-                  >
-                    Delete<span className="sr-only">, {item.amount}</span>
-                  </button>
-                </td>
+        {cashTransaction === null ? (
+          <div className="w-full grid place-items-center rounded-xl border border-gray-200 p-4 mt-12">
+            <h5 className="text-gray-400 text-base ">
+              NO CASH TRANSACTIONS AVAILABLE.
+            </h5>
+          </div>
+        ) : (
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                >
+                  Type
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Amount
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Status
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
+                  Reference
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Date
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                  <span className="sr-only">Edit</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white text-left">
+              {cashTransaction.map((item, index) => (
+                <tr key={index}>
+                  <td className="w-2/5 max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0 capitalize w-">
+                    {item.type}
+                    <dl className="font-normal lg:hidden">
+                      <dt className="sr-only sm:hidden">Status</dt>
+                      <dd className="mt-1 truncate text-gray-700 sm:hidden">
+                        {item.status}
+                      </dd>
+                      <dt className="sr-only lg:hidden">Reference</dt>
+                      <dd className="mt-1 truncate text-gray-500 lg:hidden">
+                        {item.reference}
+                      </dd>
+                    </dl>
+                  </td>
+                  <td className="px-3 py-4 text-sm text-gray-500 ">
+                    {formatNumber(item.amount)}
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell capitalize">
+                    {item.status}
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500  capitalize">
+                    {item.reference}
+                  </td>
+                  <td className="px-3 py-4 text-sm text-gray-500 capitalize">
+                    {item.date}
+                  </td>
+                  <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <button
+                      onClick={() => handleEdit(item.id)}
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Edit<span className="sr-only">, {item.amount}</span>
+                    </button>
+                  </td>
+                  <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <button
+                      onClick={() => handleDelete(item.id)}
+                      className="text-red-600 hover:text-red-900"
+                    >
+                      Delete<span className="sr-only">, {item.amount}</span>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
-        </table>
       </div>
     </div>
   );
