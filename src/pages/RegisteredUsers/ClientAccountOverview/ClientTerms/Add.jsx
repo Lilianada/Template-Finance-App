bd98aa75013761b280738ac11ef0bf55aeea6371
Lilianada <lilibest30.lb@gmail.com>
2024-03-29 07:53:19 -0400

    terms files

---
+++ src/pages/RegisteredUsers/ClientAccountOverview/ClientTerms/Add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddUserTerms() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/pages/RegisteredUsers/ClientAccountOverview/ClientTerms/TermsCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TermsCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
