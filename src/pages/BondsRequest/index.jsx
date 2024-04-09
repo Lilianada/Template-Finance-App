4a6fd29b8f772e225ef6a43719a9578a5adb750a
Lilianada <lilibest30.lb@gmail.com>
2024-04-08 20:30:57 -0400

    bonds request

--- src/pages/BondsRequest/index.jsx
+++ src/pages/BondsRequest/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import DotLoader from "../../components/DotLoader";
-import { auth, db } from "../../config/firebase";
 import { customModal } from "../../utils/modalUtils";
 import { useModal } from "../../context/ModalContext";
 import {
@@ -18,14 +17,11 @@ import {
 } from "../../config/bonds";
 import { formatNumber } from "../../config/utils";
 import { addNotification } from "../../config/notifications";
-import { doc, getDoc } from "firebase/firestore";
 
 export default function BondsRequests() {
-  //   const [bondRequests, setRequests] = useState([]);
   const [requests, setRequests] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { showModal, hideModal } = useModal();
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetchUsersRequest();
@@ -44,7 +40,57 @@ export default function BondsRequests() {
     }
   };
 
-  const handleUpdateRequest = async (userId, requestId, newStatus) => {
+  const handleUpdateRequest = (userId, requestId, newStatus) => {
+    if (newStatus === "Approved") {
+      customModal({
+        showModal,
+        title: "Are you sure?",
+        text: `You are about to approve this bonds investment.`,
+        showConfirmButton: true,
+        confirmButtonText: "Yes, approve",
+        cancelButtonText: "Cancel",
+        confirmButtonBgColor: "bg-green-600",
+        confirmButtonTextColor: "text-white",
+        cancelButtonBgColor: "bg-white",
+        cancelButtonTextColor: "text-gray-900",
+        onConfirm: () => {
+          confirmRequest(userId, requestId, newStatus);
+          hideModal();
+        },
+        onCancel: hideModal(),
+        onClose: hideModal(),
+        icon: ExclamationTriangleIcon,
+        iconBgColor: "bg-green-100",
+        iconTextColor: "text-green-600",
+        timer: 0,
+      });
+    } else {
+      customModal({
+        showModal,
+        title: "Are you sure?",
+        text: `You are about to decline this bonds investment. This action cannot be undone.`,
+        showConfirmButton: true,
+        confirmButtonText: "Yes, decline",
+        cancelButtonText: "Cancel",
+        confirmButtonBgColor: "bg-red-600",
+        confirmButtonTextColor: "text-white",
+        cancelButtonBgColor: "bg-white",
+        cancelButtonTextColor: "text-gray-900",
+        onConfirm: () => {
+          confirmRequest(userId, requestId, newStatus);
+          hideModal();
+        },
+        onCancel: hideModal(),
+        onClose: hideModal(),
+        icon: ExclamationTriangleIcon,
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-red-600",
+        timer: 0,
+      });
+    }
+  };
+
+  const confirmRequest = async (userId, requestId, newStatus) => {
     try {
       setIsLoading(true);
 
@@ -113,43 +159,6 @@ export default function BondsRequests() {
     }
   };
 
-  useEffect(() => {
-    const fetchBondRequests = async () => {
-      try {
-        setIsLoading(true);
-        const allRequests = await getBondRequests();
-        const requestsWithUserDetails = await Promise.all(
-          allRequests.map(async (request) => {
-            const userDocRef = doc(db, "users", request.userId);
-
-            // Check if the user document exists
-            const userDocSnapshot = await getDoc(userDocRef);
-
-            if (userDocSnapshot.exists()) {
-              const userDetails = userDocSnapshot.data();
-              return { ...request, userName: userDetails.fullName };
-            } else {
-              // Handle the case where the user document doesn't exist
-              return { ...request, userName: "Unknown User" };
-            }
-          })
-        );
-        setRequests(requestsWithUserDetails);
-      } catch (error) {
-        console.error("Error fetching request:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchBondRequests();
-  }, []);
-
-  const handleAddUser = () => {
-    navigate(`/dashboard/add_new_user`);
-  };
-
-
   return (
     <div className="lg:px-4">
       <div className="sm:flex sm:items-center text-left">
@@ -162,15 +171,6 @@ export default function BondsRequests() {
             email, mobile phone and user id.
           </p>
         </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={handleAddUser}
-          >
-            Create New User
-          </button>
-        </div>
       </div>
 
       <div className="mt-8 flow-root">
@@ -178,105 +178,109 @@ export default function BondsRequests() {
           <div className="my-8">
             <DotLoader />
           </div>
+        ) : !requests || requests.length === 0 ? (
+          <div className="w-full grid place-items-center rounded-xl border border-gray-200 p-4 mt-6">
+            <h5 className="text-gray-400 text-lg ">NO REQUESTS FOUND.</h5>
+          </div>
         ) : (
-            !requests || requests.length === 0 ? (
-                <div className="w-full grid place-items-center rounded-xl border border-gray-200 p-4 mt-6">
-                  <h5 className="text-gray-400 text-lg ">NO REQUESTS FOUND.</h5>
-                </div>
-              ) : (
-                <div className="-mx-4 mt-8 sm:-mx-0">
-                  <table className="min-w-full divide-y divide-gray-300 text-left">
-                    <thead>
-                      <tr>
-                        <th
-                          scope="col"
-                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                        >
-                          User Name
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
-                        >
-                          Amount
-                        </th>
-                        <th
-                          scope="col"
-                          className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-                        >
-                          Issuer Name
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-                        >
-                          Type
-                        </th>
-                        <th
-                          scope="col"
-                          className=" px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
-                        >
-                          Status
-                        </th>
-                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                          <span className="sr-only">Edit</span>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                      {requests.map((request, index) => (
-                        <tr key={index}>
-                          <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                            {request.userName}
-                            <dl className="font-normal lg:hidden">
-                              <dt className="sr-only sm:hidden">Issuer Name</dt>
-                              <dd className="mt-1 truncate text-gray-500 max-w-16">
-                                {request.issuerName}
-                              </dd>
-                            </dl>
-                          </td>
-                          <td className=" px-3 py-4 text-sm text-gray-500 ">
-                            ${formatNumber(request.amountRequested)}
-                          </td>
-                          <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell truncate">
-                            {request.issuerName}
-                          </td>
-                          <td className=" px-3 py-4 text-sm text-gray-500 ">
-                            {request.typeOfRequest}
-                          </td>
-                          <td className="px-3 py-4 text-sm text-gray-500 lg:table-cell truncate">
-                            {request.requestStatus}
-                          </td>
-                          <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <button
-                              onClick={() => handleUpdateRequest(
-                                request.userId,
-                                request.id,
-                                "Approved"
-                              )}
-                              className="text-green-600 hover:text-green-800"
-                            >
-                              Approve<span className="sr-only">, {request.userName}</span>
-                            </button>
-                          </td>
-                          <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <button
-                              onClick={() => handleUpdateRequest(
-                                request.userId,
-                                request.id,
-                                "Declined"
-                              )}
-                              className="text-red-600 hover:text-red-900"
-                            >
-                              Reject<span className="sr-only">, {request.fullName}</span>
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              )
+          <div className="-mx-4 mt-8 sm:-mx-0">
+            <table className="min-w-full divide-y divide-gray-300 text-left">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    User Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
+                  >
+                    Amount
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Issuer Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Type
+                  </th>
+                  <th
+                    scope="col"
+                    className=" px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
+                  >
+                    Status
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {requests.map((request, index) => (
+                  <tr key={index}>
+                    <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                      {request.userName}
+                      <dl className="font-normal lg:hidden">
+                        <dt className="sr-only sm:hidden">Issuer Name</dt>
+                        <dd className="mt-1 truncate text-gray-500 max-w-16">
+                          {request.issuerName}
+                        </dd>
+                      </dl>
+                    </td>
+                    <td className=" px-3 py-4 text-sm text-gray-500 ">
+                      ${formatNumber(request.amountRequested)}
+                    </td>
+                    <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell truncate">
+                      {request.issuerName}
+                    </td>
+                    <td className=" px-3 py-4 text-sm text-gray-500 ">
+                      {request.typeOfRequest}
+                    </td>
+                    <td className="px-3 py-4 text-sm text-gray-500 lg:table-cell truncate">
+                      {request.requestStatus}
+                    </td>
+                    <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <button
+                        onClick={() =>
+                          handleUpdateRequest(
+                            request.userId,
+                            request.id,
+                            "Approved"
+                          )
+                        }
+                        className="text-green-600 hover:text-green-800"
+                      >
+                        Approve
+                        <span className="sr-only">, {request.userName}</span>
+                      </button>
+                    </td>
+                    <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <button
+                        onClick={() =>
+                          handleUpdateRequest(
+                            request.userId,
+                            request.id,
+                            "Declined"
+                          )
+                        }
+                        className="text-red-600 hover:text-red-900"
+                      >
+                        Reject
+                        <span className="sr-only">, {request.fullName}</span>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
       </div>
     </div>
