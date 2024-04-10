ef1501c114f16f0c726e4404de218a034923f0f5
Lilianada <lilibest30.lb@gmail.com>
2024-04-10 08:23:02 -0400

    added logout button to dashboard list

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -12,88 +12,64 @@ import logout_icon from "../../assets/images/logout.png";
 import notification_icon from "../../assets/images/notification.png";
 import settings_icon from "../../assets/images/settings.png"; 
 import { Link } from "react-router-dom";
-import { useModal } from "../../context/ModalContext";
-import { auth, db } from "../../config/firebase";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { addLogNotification } from "../../config/admin";
+import { auth } from "../../config/firebase";
+import { ExclamationTriangleIcon } from "@heroicons/react/20/solid";
+import CustomModal from "../../components/CustomModal";
 
-const menus = [
-  {
-    title: "Registered Users",
-    icon: account_details,
-    href: "/dashboard/registered_users",
-  },
-  {
-    title: "Cash Deposits",
-    icon: transactions_icon,
-    href: "/dashboard/cash_deposits",
-  },
-  {
-    title: "Bonds",
-    icon: bonds_icon,
-    href: "/dashboard/bonds",
-  },
-  {
-    title: "Fixed Terms",
-    icon: fixedTerm_icon,
-    href: "/dashboard/fixed_term_deposits",
-  },
-  {
-    title: "IPOs",
-    icon: ipos_icon,
-    href: "/dashboard/ipos",
-  },
-  {
-    title: "Notification",
-    icon: notification_icon,
-    href: "/dashboard/notifications",
-  },
-  {
-    title: "Settings",
-    icon: settings_icon,
-    href: "/dashboard/settings",
-  },
-  {
-    title: "Logout",
-    icon: logout_icon,
-    href: {},
-  },
-];
 
 export default function Dashboard() {
-  const {showModal, hideModal} = useModal();
   const [isLoading, setIsLoading] = useState(false);
-  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    setIsLoading(true);
-
-    try {
-      const user = auth.currentUser;
-
-      if (user) {
-        // Fetch the user's details from Firestore
-        const userRef = doc(db, "users", user.uid);
-        const userDoc = await getDoc(userRef);
-        if (userDoc.exists()) {
-          await updateDoc(userRef, { isLoggedIn: false });
-          await addLogNotification(userRef, user);
-        }
-        await auth.signOut();
-      }
-
-      setIsLoading(false);
-      navigate("/");
-    } catch (error) {
-      setIsLoading(false);
-      console.error("Error signing out:", error);
-    }
-  };
+  
+  const menus = [
+    {
+      title: "Registered Users",
+      icon: account_details,
+      href: "/dashboard/registered_users",
+    },
+    {
+      title: "Cash Deposits",
+      icon: transactions_icon,
+      href: "/dashboard/cash_deposits",
+    },
+    {
+      title: "Bonds",
+      icon: bonds_icon,
+      href: "/dashboard/bonds",
+    },
+    {
+      title: "Fixed Terms",
+      icon: fixedTerm_icon,
+      href: "/dashboard/fixed_term_deposits",
+    },
+    {
+      title: "IPOs",
+      icon: ipos_icon,
+      href: "/dashboard/ipos",
+    },
+    {
+      title: "Notification",
+      icon: notification_icon,
+      href: "/dashboard/notifications",
+    },
+    {
+      title: "Settings",
+      icon: settings_icon,
+      href: "/dashboard/settings",
+    },
+    {
+      title: "Logout",
+      icon: logout_icon,
+      onClick : () => setOpenModal(true),
+    },
+  ];
+  
   return (
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-4">
       {menus.map((item, index) => (
         <li
+        onClick={item.onClick}
           key={index}
           className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow hover:scale-95 transition-all ease-in-out"
         >
@@ -112,6 +88,36 @@ export default function Dashboard() {
           </Link>
         </li>
       ))}
+      <CustomModal
+          open={openModal}
+          onClose={() => setOpenModal(false)}
+          title="Signout"
+          description="Are you sure you want to sign out of your account?"
+          showConfirmButton={true}
+          confirmButtonText="Sign Out"
+          cancelButtonText="Cancel"
+          confirmButtonBgColor="bg-red-600"
+          confirmButtonTextColor="text-white"
+          onConfirm={() => {
+            setIsLoading(true);
+            auth
+              .signOut()
+              .then(() => {
+                setIsLoading(false);
+                navigate("/");
+              })
+              .catch((error) => {
+                setIsLoading(false);
+                console.error("Error signing out:", error);
+              });
+          }}
+          onCancel={() => setOpenModal(false)}
+          Icon={ExclamationTriangleIcon} // Your Icon Component
+          iconBgColor="bg-red-100"
+          buttonBgColor="bg-red-600"
+          iconTextColor="text-red-600"
+          loading={isLoading}
+        />
     </ul>
   );
 }
