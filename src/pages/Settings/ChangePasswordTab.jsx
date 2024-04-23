e8a4e19778772ad5c6facb09547485bbe0442455
Lilianada <lilibest30.lb@gmail.com>
2024-04-23 03:56:08 -0400

    change password tab

--- src/pages/Settings/ChangePasswordTab.jsx
+++ src/pages/Settings/ChangePasswordTab.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import {
   EmailAuthProvider,
   getAuth,
@@ -10,11 +10,26 @@ import {
   ExclamationCircleIcon,
   EyeIcon,
   EyeSlashIcon,
+  QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
 import DotLoader from "../../components/DotLoader";
 import { fetchPasswordPolicySetting } from "../../config/settings";
 import { customModal } from "../../utils/modalUtils";
 import { useModal } from "../../context/ModalContext";
+import { Popover, Transition } from "@headlessui/react";
+
+const strongPolicyRequirements = [
+  { name: "Minimum of 6 characters" },
+  { name: "One uppercase letter" },
+  { name: "One lowercase letter" },
+  { name: "At least one number" },
+  { name: "At least one special character (!@#$%^&*)" },
+];
+
+const requirements = [
+  { name: "No spaces allowed" },
+  { name: "Minimum of 6 characters" },
+];
 
 export default function ChangePassword() {
   const { showModal } = useModal();
@@ -165,8 +180,50 @@ export default function ChangePassword() {
               <h3 className="text-base font-semibold leading-6 text-gray-900">
                 Change Password
               </h3>
-              <p className="mt-1 text-sm text-gray-500">
+              <p className="mt-1 text-sm text-gray-500 inline-flex gap-2">
                 Update the password associated with your account.
+                <Popover className="relative">
+                <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                  <span>
+                    <QuestionMarkCircleIcon
+                      className="h-5 w-5 text-indigo-600"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Popover.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-min -translate-x-1/2 px-4">
+                  <ul className="w-56 shrink rounded-xl bg-white p-3 text-sm font-medium leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5 text-left">
+                      { isStrongPasswordPolicy ? (
+                        strongPolicyRequirements.map((item) => (
+                          <li
+                            key={item.name}
+                            className="block hover:text-indigo-600"
+                          >
+                            {item.name}
+                          </li>
+                        ))) : (
+                      requirements.map((item) => (
+                        <li
+                          key={item.name}
+                          className="block hover:text-indigo-600"
+                        >
+                          {item.name}
+                        </li>
+                      )))
+                    }
+                    </ul>
+                  </Popover.Panel>
+                </Transition>
+              </Popover>
               </p>
             </div>
 
